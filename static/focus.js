02036ce40f32a8a62e40b739f5ee17996aca6214
POLINA <polinaionina67@gmail.com>
2026-04-28 18:31:36 +0500

    1

--- static/focus.js
+++ static/focus.js
@@ -24,18 +24,19 @@ function run() {
 run();
 
 function sendRequest(url, callback) {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                callback(JSON.parse(xhr.response));
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", url, true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 200) {
+                    callback(JSON.parse(xhr.response));
+                }
             }
-        }
-    };
-
-    xhr.send();
+        };
+        
+        xhr.send();
+    });
 }
 
 function reqsToMap(requisites) {
